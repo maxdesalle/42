78cd63ec9f89e825e5f240556e9dc65bb11824cd
maxdesalle <maxdesalle@pm.me>
2020-11-16 16:44:43 +0100

    update header file

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mdesalle <mdesalle@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/13 10:10:43 by mdesalle          #+#    #+#             */
-/*   Updated: 2020/11/16 14:37:25 by mdesalle         ###   ########.fr       */
+/*   Updated: 2020/11/16 16:44:30 by mdesalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,18 +24,14 @@ int     ft_isalnum(int c);
 char    *ft_itoa(int n);
 int     ft_strncmp(const char *s1, const char *s2, size_t n);
 int     ft_isalpha(int c);
-void    ft_putchar(char c);
-int     ft_strcmp(const char *s1, const char *s2);
 int     ft_isascii(int c);
-char    *ft_strstr(const char *haystack, const char *needle);
 int     ft_isdigit(int c);
 void    ft_putnbr(int n);
 size_t  ft_strlen(const char *s);
 int     ft_tolower(int c);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char const *s, int fd);
-void	ft_putendl_fd(char const *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-char	*strchr(const char *s, int c);
+char	*ft_strchr(const char *s, int c);
 
 #endif
