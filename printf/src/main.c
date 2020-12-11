7b50822b1c2de2e4e3c54f1280aa430f8e7f50a8
maxdesalle <maxdesalle@pm.me>
2020-12-11 09:53:02 +0100

    quick fix of header in main

--- printf/src/main.c
+++ printf/src/main.c
@@ -6,11 +6,11 @@
 /*   By: mdesalle <mdesalle@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/10 10:33:39 by mdesalle          #+#    #+#             */
-/*   Updated: 2020/12/11 08:31:03 by mdesalle         ###   ########.fr       */
+/*   Updated: 2020/12/11 09:52:33 by mdesalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "include/printf.h"
+#include "../include/printf.h"
 
 int	main()
 {
