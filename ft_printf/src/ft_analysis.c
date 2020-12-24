7edf4c6ae3ca5126d82f2c998ed5bb1c10db79a9
maxdesalle <maxdesalle@pm.me>
2020-12-24 09:52:09 +0100

    having a width larger than 9 now works correctly

--- ft_printf/src/ft_analysis.c
+++ ft_printf/src/ft_analysis.c
@@ -6,7 +6,7 @@
 /*   By: mdesalle <mdesalle@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/03 08:51:16 by mdesalle          #+#    #+#             */
-/*   Updated: 2020/12/24 09:26:38 by mdesalle         ###   ########.fr       */
+/*   Updated: 2020/12/24 09:50:53 by mdesalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,8 +48,9 @@ static void	ft_flag(char *str, t_list *box)
 	box->fplus = 0;
 	box->fminus = 0;
 	box->fspace = 0;
-	if (ft_strchr(str, '0'))
-		box->fzero += 1;
+	while (*str != '\0' && (!(*str >= '1' && *str <= '9')))
+		if (*str++ == '0')
+			box->fzero += 1;
 	else if (ft_strchr(str, '+'))
 		box->fplus += 1;
 	else if (ft_strchr(str, '-'))
