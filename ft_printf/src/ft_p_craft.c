12a48fd439284bbab9cdba1ff294cc4d8ba8fc9f
maxdesalle <maxdesalle@pm.me>
2021-01-11 11:27:16 +0100

    removed comments

--- ft_printf/src/ft_p_craft.c
+++ ft_printf/src/ft_p_craft.c
@@ -6,7 +6,7 @@
 /*   By: mdesalle <mdesalle@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/23 11:01:30 by mdesalle          #+#    #+#             */
-/*   Updated: 2021/01/11 11:19:10 by mdesalle         ###   ########.fr       */
+/*   Updated: 2021/01/11 11:23:50 by mdesalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ static void	ft_print_pointer(unsigned long long x,
 	{
 		ft_super_putchar(box->width - ft_strlen(basechar) - 2, ' ', box);
 		if (box->width > 2 && box->fdot == 1 && box->precision == 0 && !x)
-                        ft_putchar(' ', box);
+			ft_putchar(' ', box);
 		ft_putstr_hexa("0x", 1, box);
 		if (!(box->fdot == 1 && box->precision == 0 && !x))
 			ft_putstr_hexa(basechar, x, box);
@@ -84,7 +84,5 @@ void		ft_p_craft(unsigned long long x, t_list *box)
 		x = 0;
 	len = ft_baselen(x);
 	base = "0123456789abcdef";
-/*	if (box->fdot == 1 && box->width > 2 && !x)
-		ft_putchar(' ', box);*/
 	ft_dispatcher(len, base, x, box);
 }
