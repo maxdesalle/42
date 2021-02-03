a6c45fea0d00b6ff328b6620fcef5f28ad147ddd
maxdesalle <maxdesalle@pm.me>
2021-02-03 09:34:43 +0100

    re-added files to correct D/d bug

---
+++ cub3D/include/cub3d.h
@@ -0,0 +1,111 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdesalle <mdesalle@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/27 10:51:18 by mdesalle          #+#    #+#             */
+/*   Updated: 2021/02/03 09:31:21 by mdesalle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <math.h>
+# include <fcntl.h>
+# include <limits.h>
+
+# define BUFFER_SIZE 32
+
+/*
+** linked list used for get_next_line.c
+*/
+
+typedef struct	s_list
+{
+	int	check;
+	int	reader;
+}				t_list;
+
+/*
+** linked list used accross all the code to store the necessary info about
+** the map, textures...
+*/
+
+typedef struct	q_list
+{
+	int	Rx;
+	int	Ry;
+	int	i;
+	int	**map;
+	int	error;
+	int	nboflines;
+	int	linelength;
+	int	counter;
+	char	*NO;
+	char	*SO;
+	char	*WE;
+	char	*EA;
+	char	*S;
+}				v_list;
+
+/*
+** ft_resolution.c
+*/
+
+void			ft_resolution(char *line, v_list *cube);
+
+/*
+** ft_textures.c
+*/
+
+void			ft_textures(char *line, v_list *cube);
+
+/*
+** ft_map.c
+*/
+
+int				ft_map_allocator(v_list *cube);
+int				ft_array_insert(char *line, v_list *cube);
+
+/*
+** ft_mapcheck.c
+*/
+
+int				ft_mapsize(char *line, v_list *cube);
+int				ft_mapvalid(char *line);
+
+/*
+** get_next_line.c
+*/
+
+int				get_next_line(int fd, char **line);
+
+/*
+** get_next_line_utils.c
+*/
+
+char			*ft_substr(char const *s, unsigned int start, size_t len);
+char			*ft_strjoin(char const *s1, char const *s2);
+char			*ft_strcat_alpha(char *dest, char *src, int len);
+
+/*
+** ft_error_check.c
+*/
+
+int				ft_argnamecheck(char *argv);
+int				ft_argsavecheck(char *argv);
+int				ft_error(int option);
+
+/*
+** ft_utilities_one.c
+*/
+
+int				ft_atoi_char(char c);
+size_t			ft_strlen(const char *s);
+
+#endif
---
+++ cub3D/src/cub3d.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdesalle <mdesalle@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/27 10:54:16 by mdesalle          #+#    #+#             */
+/*   Updated: 2021/02/03 09:31:53 by mdesalle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/cub3d.h"
+
+static int	ft_map_analytics(char *mapfile, v_list *cube)
+{
+	int	fd;
+	char	*line;
+
+	if ((fd = open(mapfile, O_RDONLY)) == -1)
+		return (ft_error(3));
+	while (get_next_line(fd, &line) == 1)
+	{
+		if (ft_mapvalid(line) == 1)
+			ft_array_insert(line, cube);
+		free(line);
+	}
+	close (fd);
+	return (0);
+}
+
+/*
+** uses get_next_line to parse the map.cub file and store it inside a set of
+** linked lists.
+*/
+
+static int	ft_analytics(char *mapfile, v_list *cube)
+{
+	int		fd;
+	char	*line;
+
+	cube->nboflines = 0;
+	cube->linelength = 0;
+	if ((fd = open(mapfile, O_RDONLY)) == -1)
+		return (ft_error(3));
+	while (get_next_line(fd, &line) == 1)
+	{
+		ft_resolution(line, cube);
+		ft_textures(line, cube);
+		ft_mapsize(line, cube);
+		free(line);
+	}
+	close(fd);
+	ft_map_allocator(cube);
+	ft_map_analytics(mapfile, cube);
+	return (0);
+}
+
+/*
+** main function. If there are two arguments given (./cub3D map.cub), the
+** namecheck functions checks that the map file has the right format (.cub). If
+** so, the function calls the initialization function to initialize all the
+** flags.
+**
+** if there are 3 arguments, it checks the file extension and the
+** save argument. Otherwise, it returns an error through the ft_error function.
+*/
+
+int		main(int argc, char **argv)
+{
+	v_list	cube;
+
+	if (argc == 2 && ft_argnamecheck(argv[1]))
+		ft_analytics(argv[1], &cube);
+	else if (argc == 3 && ft_argnamecheck(argv[1])
+			&& ft_argsavecheck(argv[2]))
+		ft_analytics(argv[1], &cube);
+	else
+		return (ft_error(0));
+	return (0);
+}
