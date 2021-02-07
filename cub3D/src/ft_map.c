5450b0f43e5db6f5b8ba52993702aae98352696d
maxdesalle <maxdesalle@pm.me>
2021-02-07 10:58:22 +0100

    int map array now works perfectly

--- cub3D/src/ft_map.c
+++ cub3D/src/ft_map.c
@@ -6,7 +6,7 @@
 /*   By: mdesalle <mdesalle@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 08:38:21 by mdesalle          #+#    #+#             */
-/*   Updated: 2021/02/05 13:51:40 by mdesalle         ###   ########.fr       */
+/*   Updated: 2021/02/07 10:37:11 by mdesalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,27 @@ int	ft_map_free(v_list *cube)
 
 	i = 0;
 	while (cube->counter > 0)
+	{
 		free(cube->map[cube->counter]);
+		cube->counter--;
+	}
 	return (0);
 }
 
 int	ft_array_insert(char *line, v_list *cube)
 {
 	int	i;
+	int	j;
 
 	i = 0;
-	while (line[i] != '\0' && line[i] != '\n')
+	j = 0;
+	while (line[j] != '\0' && line[j] != '\n')
 	{
-		if (line[i] == ' ')
-			i++;
-		cube->map[cube->counter][i] = ft_atoi_char(line[i]);
+		if (line[j] == ' ')
+			j++;
+		cube->map[cube->counter][i] = ft_atoi_char(line[j]);
 		i++;
+		j++;
 	}
 	cube->counter += 1;
 	return (0);
