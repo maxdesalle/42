e336e35b7cd810c55775a4c08f712a7bee4f9046
Maxime Desalle <maxdesalle@Maximes-MacBook-Air.local>
2021-02-10 11:00:18 +0100

    just saving my advancements

--- cub3D/src/ft_initiation.c
+++ cub3D/src/ft_initiation.c
@@ -12,10 +12,19 @@
 
 #include "../include/cub3d.h"
 
+int	ft_compute(v_list *cube)
+{
+	ft_computation(cube, 
+
 int	ft_initiation(v_list *cube)
 {
 	if (!(cube->mlx_ptr = mlx_init()))
 		return (ft_error(6));
 	mlx_get_screen_size(cube->mlx_ptr, &cube->Sx, &cube->Sy);
+	if (cube->Rx > cube->Sx)
+		cube->Rx = cube->Sx;
+	if (cube->Ry > cube->Sy)
+		cube->Ry = cube->Sy;
+	ft_compute(cube);
 	return (0);
 }
