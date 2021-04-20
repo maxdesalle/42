f314d6f5773a6a84a35b41e6af8a0d8e28896489
maxdesalle <maxdesalle@pm.me>
2021-04-20 09:04:03 +0000

    trying something

--- ft_server/srcs/mysql_setup.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE wordpress;
-
-GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost';
-
-FLUSH PRIVILEGES;
-
-update mysql.user set plugin = 'mysql_native_password' where user='root';
--- ft_server/srcs/start.sh
+++
@@ -1,4 +0,0 @@
-service php7.3-fpm start
-service nginx start
-service mysql start
-sleep infinity
