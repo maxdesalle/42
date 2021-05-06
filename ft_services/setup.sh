0c949a4c5546c5f0535e5f75725979b511686fb8
maxdesalle <maxdesalle@pm.me>
2021-05-06 10:04:34 +0000

    Added deployment files and dockerfiles

--- ft_services/setup.sh
+++ ft_services/setup.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
 minikube start --driver=virtualbox
+
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/metallb.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
+for service in phpmyadmin influxdb mysql grafana wordpress ftps nginx 
+do
+	docker build -t ${service}_custom srcs/$service/
+	kubectl apply -f srcs/$service/$service.yaml
+done
+
+kubectl apply -f srcs/metallb/metallb.yaml 
+
+minikube dashboard
