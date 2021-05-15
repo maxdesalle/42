d1b73b123e4983e93d0a1c8aace68f9c01228bce
maxdesalle <maxdesalle@pm.me>
2021-05-15 17:20:12 +0200

    added some nice little colors

--- ft_services/setup.sh
+++ ft_services/setup.sh
@@ -8,6 +8,8 @@ PURPLE="\e[95m"
 CYAN="\e[96m"
 WHITE="\e[97m"
 
+echo -en $WHITE
+
 minikube delete
 minikube start --driver=docker --cpus=2
 minikube addons enable metrics-server
@@ -16,7 +18,6 @@ minikube addons enable metallb
 
 if [ -e srcs/metallb/metallb-configmap.yaml ]
 then
-	echo -en $WHITE
 	kubectl apply -f srcs/metallb/metallb-configmap.yaml
 	echo -en $GREEN
 	echo "Successfully deployed metallb configuration map"
@@ -29,29 +30,40 @@ kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manife
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
-for service in phpmyadmin influxdb mysql grafana wordpress ftps nginx metallb
+for service in phpmyadmin influxdb mysql grafana wordpress ftps nginx
 do
 	if [ -e srcs/$service/Dockerfile ]
 	then
-		echo -en $WHITE
+		echo -en $PURPLE
+		echo "Building $service..."
+		echo -en $YELLOW
 		docker build -t my_${service} srcs/$service/
 		echo -en $GREEN
 		echo "Successfully set up the Docker container for $service"
 		echo -e $WHITE
 	elif [ -e srcs/$service/srcs/$service-deployment.yaml ]
 	then
+		echo -en $PURPLE
+		echo "Deploying $service..."
+		echo -en $YELLOW
 		kubectl apply -f srcs/$service/srcs/$service-deployment.yaml
 		echo -en $GREEN
 		echo "Successfully deployed $service"
 		echo -en $WHITE
 	elif [ -e srcs/$service/srcs/$service-service.yaml ]
 	then
+		echo -en $PURPLE
+		echo "Exposing $service..."
+		echo -en $YELLOW
 		kubectl apply -f srcs/$service/srcs/$service-service.yaml
 		echo -en $GREEN
 		echo "Successfully exposed $service"
 		echo -en $WHITE
 	elif [ -e srcs/$service/srcs/$service-volume.yaml ]
 	then
+		echo -en $PURPLE
+		echo "Creating $service volume..."
+		echo -en $YELLOW
 		kubectl apply -f srcs/$service/srcs/$service-volume.yaml
 		echo -en $GREEN
 		echo "Successfully created $service volume"
@@ -59,4 +71,6 @@ do
 	fi
 done
 
-minikube dashboard
+echo -en $GREEN
+echo "Kubernetes cluster is ready to use!"
+echo -en $WHITE
