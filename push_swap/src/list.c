306d7d2b0c0844436943de46f02823b1b3033b20
maxdesalle <maxdesalle@pm.me>
2021-06-10 21:58:46 +0200

    doubly circular linked list now finally works

--- push_swap/src/list.c
+++ push_swap/src/list.c
@@ -6,7 +6,7 @@
 /*   By: mdesalle <mdesalle@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/06 10:52:44 by mdesalle          #+#    #+#             */
-/*   Updated: 2021/06/10 14:42:42 by mdesalle         ###   ########.fr       */
+/*   Updated: 2021/06/10 21:52:45 by mdesalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ static int	d_check(t_node *head, int value, int i)
 	return (1);
 }
 
-static void	create(t_node **head, t_node **tail, int value)
+static void	create(t_node **head, t_node **tail, int value, int i)
 {
 	t_node	*tmp;
 	t_node	*result;
@@ -58,8 +58,11 @@ static void	create(t_node **head, t_node **tail, int value)
 		*head = result;
 		return ;
 	}
-	while (tmp->next != NULL)
+	while (tmp->next != NULL && i > 0)
+	{
 		tmp = tmp->next;
+		i--;
+	}
 	tmp->next = result;
 	result->prev = tmp;
 	*tail = result;
@@ -75,7 +78,7 @@ void	save(t_node **head, t_node **tail, int argc, char **argv)
 	while (i < argc)
 	{
 		if (num_check(argv[i]) && d_check(*head, ft_atoi(argv[i]), i))
-			create(head, tail, ft_atoi(argv[i]));
+			create(head, tail, ft_atoi(argv[i]), i);
 		i++;
 	}
 }
