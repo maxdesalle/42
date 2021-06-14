9ba9524d4be082894ecef63515db9c9fb1c5e061
maxdesalle <maxdesalle@pm.me>
2021-06-14 22:56:57 +0200

    added ex01

---
+++ python-django-piscine/day01/ex01/numbers.py
@@ -0,0 +1,9 @@
+def main():
+    for line in open("numbers.txt", "r"):
+        value = line.split(",")
+    for i in value:
+        i = int(i)
+        print(i)
+
+if __name__ == '__main__':
+    main()
