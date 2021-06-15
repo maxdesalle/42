e812333ce3261d7115422b9504f9699408be36a5
maxdesalle <maxdesalle@pm.me>
2021-06-15 08:58:34 +0200

    updated state.py

--- python-django-piscine/day01/ex04/state.py
+++ python-django-piscine/day01/ex04/state.py
@@ -1,5 +1,19 @@
 import sys
 
+def search_cities(city, capital_cities):
+    for key, value in capital_cities.items():
+        if value == city:
+            return key
+    else:
+        print("Unknown capital city")
+        return
+
+def search_states(value, states):
+    for i, key in states.items():
+        if value == key:
+            print(i)
+            return
+
 def state(argv):
     if len(argv) != 2:
         return
@@ -15,17 +29,7 @@ def state(argv):
             "NJ": "Trenton",
             "CO": "Denver"
             }
-    for key, value in capital_cities.items():
-        if value == argv[1]:
-            value = key
-            break
-    else:
-        print("Unknown capital city")
-        return
-    for i, key in states.items():
-        if value == key:
-            print(i)
-            return
+    search_states(search_cities(argv[1], capital_cities), states)
 
 if __name__ == '__main__':
     state(sys.argv)
