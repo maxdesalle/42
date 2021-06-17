46cf95a77db031b1a780dc01c60faa298e3f6fb6
maxdesalle <maxdesalle@pm.me>
2021-06-17 15:36:04 +0200

    fixed ex02

--- python-django-piscine/day03/ex02/request_wikipedia.py
+++ python-django-piscine/day03/ex02/request_wikipedia.py
@@ -5,12 +5,16 @@ def request_api(argv):
     
     if (len(argv) != 2):
         return
-
-    r = requests.get('https://fr.wikipedia.org/w/api.php?action=parse&page={}&prop=wikitext&formatversion=2&format=json&redirects&limit=1'.format(argv[1]))
-    r = json.loads(r.text)
-    r = dewiki.from_string(r['parse']['wikitext'])
-    print(r)
-    return
+    
+    try:
+        r = requests.get('https://en.wikipedia.org/w/api.php?action=parse&page={}&prop=wikitext&formatversion=2&format=json&redirects&limit=1'.format(argv[1]))
+        r = json.loads(r.text)
+        r = dewiki.from_string(r['parse']['wikitext'])
+        file_name = argv[1].replace(" ", "_")
+        with open (file_name + '.wiki', 'w') as w:
+            w.write(r)
+    except:
+        print("Error\nPlease try again with a valid search result")
 
 
 if __name__ == '__main__':
