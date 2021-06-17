c670b4c8b9c2f23a3df434c8ac0bd58b2a44e669
maxdesalle <maxdesalle@pm.me>
2021-06-17 11:54:53 +0200

    added a fancy sed

--- python-django-piscine/day03/ex01/my_script.sh
+++ python-django-piscine/day03/ex01/my_script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pip3 --version
+pip3 --version | sed 's/from.*//'
 python3 -m venv local_lib
 source local_lib/bin/activate
 pip3 install git+https://github.com/jaraco/path.git --log local_lib/installation.log --upgrade
