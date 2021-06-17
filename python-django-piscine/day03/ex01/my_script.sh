5203d609069daaa35591fdaa5d76df2fef38e891
maxdesalle <maxdesalle@pm.me>
2021-06-17 21:54:48 +0200

    fixed ex01 and added requirement

--- python-django-piscine/day03/ex01/my_script.sh
+++ python-django-piscine/day03/ex01/my_script.sh
@@ -4,7 +4,7 @@ pip3 --version | sed 's/from.*//'
 python3 -m venv local_lib
 source local_lib/bin/activate
 pip3 install git+https://github.com/jaraco/path.git --log local_lib/installation.log --upgrade
-if [ $? != 0 ]
+if [ $? != -1 ]
 then
 	python3 my_program.py
 fi
