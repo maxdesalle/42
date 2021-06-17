f4a96f4ace2ea2f8cc1e5d0050092737cac69a2e
maxdesalle <maxdesalle@pm.me>
2021-06-17 14:57:00 +0200

    added ex02

--- python-django-piscine/day03/ex02/request_wikipedia.py
+++ python-django-piscine/day03/ex02/request_wikipedia.py
@@ -3,19 +3,14 @@ import requests, json, dewiki, sys
 
 def request_api(argv):
     
-    test = {
-        "format": "json",
-        "action": "query",
-        "generator": "prefixsearch",
-        "gpslimit": "1",
-        "prop": "revisions",
-        "rvprop": "content",
-        "redirects": "",
-        "gpssearch": "Elon Musk"
-    }
+    if (len(argv) != 2):
+        return
 
-    r = requests.get('https://en.wikipedia.org/w/api.php', test)
-    print(r.text)
+    r = requests.get('https://fr.wikipedia.org/w/api.php?action=parse&page={}&prop=wikitext&formatversion=2&format=json&redirects&limit=1'.format(argv[1]))
+    r = json.loads(r.text)
+    r = dewiki.from_string(r['parse']['wikitext'])
+    print(r)
+    return
 
 
 if __name__ == '__main__':
