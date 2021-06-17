77a371171c7092489c8556cd6dd8dfb84a9e090a
maxdesalle <maxdesalle@pm.me>
2021-06-17 13:24:56 +0200

    added return protection ex01

--- python-django-piscine/day03/ex01/my_script.sh
+++ python-django-piscine/day03/ex01/my_script.sh
@@ -4,4 +4,7 @@ pip3 --version | sed 's/from.*//'
 python3 -m venv local_lib
 source local_lib/bin/activate
 pip3 install git+https://github.com/jaraco/path.git --log local_lib/installation.log --upgrade
-python3 my_program.py
+if [ $? != 0 ]
+then
+	python3 my_program.py
+fi
--- python-django-piscine/day03/ex02/request_wikipedia.py
+++ python-django-piscine/day03/ex02/request_wikipedia.py
@@ -1,6 +1,22 @@
 import requests, json, dewiki, sys
 
 
+def request_api(argv):
+    
+    test = {
+        "format": "json",
+        "action": "query",
+        "generator": "prefixsearch",
+        "gpslimit": "1",
+        "prop": "revisions",
+        "rvprop": "content",
+        "redirects": "",
+        "gpssearch": "Elon Musk"
+    }
+
+    r = requests.get('https://en.wikipedia.org/w/api.php', test)
+    print(r.text)
+
 
 if __name__ == '__main__':
     request_api(sys.argv)
