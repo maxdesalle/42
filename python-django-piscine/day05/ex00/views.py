462c1a2fadcf4b0ff87d1a5275567a04ffdf440b
maxdesalle <maxdesalle@pm.me>
2021-06-21 18:34:05 +0200

    updated views for ex00

--- python-django-piscine/day05/ex00/views.py
+++ python-django-piscine/day05/ex00/views.py
@@ -3,16 +3,32 @@ from django.http import HttpResponse
 import psycopg2
 
 def init(request):
-    
+   
     table = """
-        CREATE TABLE ex00_movies (
-            title VARCHAR(64) NOT NULL,
-            episode_nb SERIAL PRIMARY KEY,
-            opening_crawl TEXT
-            director VARCHAR(32) NOT NULL,
-            producer VARCHAR(128) NOT NULL,
-            release_date DATE
-        )
-    """
+        CREATE TABLE IF NOT EXISTS ex00_movies (
+        	title VARCHAR(64) UNIQUE NOT NULL,
+			episode_nb INTEGER PRIMARY KEY,
+			opening_crawl TEXT,
+			director VARCHAR(32) NOT NULL,
+			producer VARCHAR(128) NOT NULL,
+			release_date DATE NOT NULL
+            )
+            """
 
-    return HttpResponse('<h1>It works!</h1>')
+    DB_HOST = "localhost"
+    DB_NAME = "djangotraining"
+    DB_USER = "djangouser"
+    DB_PASS = "secret"
+
+    try:
+        conn = psycopg2.connect(database=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+        curr = conn.cursor()
+        curr.execute(table)
+        conn.commit()
+        conn.close()
+        curr.close()
+
+        return HttpResponse('OK')
+
+    except Exception as e:
+        return HttpResponse('Error: {}'.format(e))
