8ea63a6376ae3d6759d0af9ee7e9a7d4ee18df20
maxdesalle <maxdesalle@pm.me>
2021-06-23 16:17:37 +0200

    added some fancy styling because it\'s cool

--- python-django-piscine/day06/ex/models.py
+++ python-django-piscine/day06/ex/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from django.forms import ModelForm
 
 class Tip(models.Model):
-    content = models.TextField(help_text="Your tip")
-    author = models.CharField(max_length=64, help_text="Your username")
-    date = models.DateField(null=True, help_text="Today's date")
+    content = models.TextField()
+    author = models.CharField(max_length=64)
+    date = models.DateField(null=True)
