8a724ee1014c7ff4380b35889b2fd051abfcd9f3
maxdesalle <maxdesalle@pm.me>
2021-06-24 12:04:43 +0200

    first push for day08

---
+++ python-django-piscine/day08/app/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from .models import Flash
+from .forms import FlashForm
+
+
+def homepage(request):
+
+    data = Flash.objects.all()
+    context = {"flash": data}
+    
+    if request.method == 'POST':
+        form = FlashForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('http://127.0.0.1:8000')
+        else:
+            form = FlashForm()
+            context.update({'form': form})
+            context.update({'error': 'An error occurred, please try again with a different image'})
+            return render(request, "homepage.html", context)
+    else:
+        form = FlashForm()
+        context.update({'form': form})
+        return render(request, "homepage.html", context)
+
+    return render(request, 'homepage.html')
+
+
+def objdelete(request, id):
+    Flash.objects.filter(id=id).delete()
+    return redirect('http://127.0.0.1:8000')
