c38b8b1dfeef2de6211efbcd8240b7c22584455f
maxdesalle <maxdesalle@pm.me>
2021-06-25 11:46:00 +0200

    just saving

--- python-django-piscine/day08/setup.sh
+++ python-django-piscine/day08/setup.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 # Some nice little colors
 GREEN='\033[0;32m'
 BLUE='\033[0;34m'
@@ -15,29 +14,56 @@ echo " |  _| | | (_| \__ \ | | \ V  V / (_| | | |"
 echo " |_|   |_|\__,_|___/_| |_|\_/\_/ \__,_|_|_|"
 echo $WHITE
 
+# Virtual environment setup
+echo $BLUE
+echo "Setting up the virtual environment"
+echo $WHITE
+python3 -m venv .virtualenv > /dev/null
+source .virtualenv/bin/activate > /dev/null
+pip3 install -r requirements.txt > /dev/null
+echo $GREEN
+echo "Successfully set up the virtual environment!"
+
+# Brew setup
 echo $BLUE
 echo "Installing Brew..."
 echo $WHITE
-curl -s https://brew.42.fr/ | bash &> /dev/null
+curl -s https://brew.42.fr/ | bash > /dev/null
 echo $GREEN
 echo "Successfully installed Brew!"
-echo ""
+
+# Nginx installation
 echo $BLUE
 echo "Installing Nginx..."
 echo $WHITE
-brew install nginx &> /dev/null
+brew install nginx > /dev/null
 echo $GREEN
 echo "Successfully installed Nginx!"
-echo ""
+
+# Nginx setup
 echo $BLUE
 echo "Setting up Nginx..."
 echo $WHITE
-mkdir -p ~/.brew/etc/nginx/sites-available &> /dev/null
-mkdir -p ~/.brew/etc/nginx/sites-enabled &> /dev/null
-cp nginx/default ~/.brew/etc/nginx/sites-available &> /dev/null
-ln -s ~/.brew/etc/nginx/sites-available/default ~/.brew/etc/nginx/sites-enabled/ &> /dev/null
+cp nginx/nginx.conf ~/.brew/etc/nginx/ > /dev/null
 echo $GREEN
 echo "Successfully installed Nginx!"
-echo ""
+
+# Nginx sart up
+echo $BLUE
+echo "Starting up Nginx..."
+echo $WHITE
+brew services restart nginx > /dev/null
+echo $GREEN
+echo "Successfully started Nginx!"
+
+# Ready message
 echo $GREEN
 echo "FlashWall is ready! Get started here: https://127.0.0.1"
+
+# Gunicorn sart up
+echo $BLUE
+echo "Starting up Gunicorn..."
+echo $GREEN
+echo "Successfully started Gunicorn!"
+echo $WHITE
+gunicorn -c gunicorn/gunicorn.conf.py project.wsgi:application > /dev/null
