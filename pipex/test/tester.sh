aab2057c75185e9fe7a6dbdac7fd49291f532c77
maxdesalle <maxdesalle@pm.me>
2021-08-04 09:30:32 +0200

    updated tester

--- pipex/test/tester.sh
+++ pipex/test/tester.sh
@@ -12,11 +12,23 @@ WHITE="\e[97m"
 echo -n "baracuda\ncrocodile\nelephant\n" > file1
 echo -n "baracuda\ncrocodile\nelephant\n" > file2
 
-< README.md ls -la | wc > outfile1
-./pipex README.md "ls -la" wc outfile2
+< README.md ls -la | grep pipex > outfile1
+./pipex README.md "ls -la" "grep pipex" outfile2
 diff outfile1 outfile2
+rm -f outfile1
+rm -f outfile2
+
+< README.md grep is | wc -l > outfile1
+./pipex README.md "grep is" "wc -l" outfile2
+diff outfile1 outfile2
+rm -f outfile1
+rm -f outfile2
+
+< README.md cat | wc -w > outfile1
+./pipex README.md cat "wc -w" outfile2
+diff outfile1 outfile2
+rm -f outfile1
+rm -f outfile2
 
 rm -f file1
 rm -f file2
-rm -f outfile1
-rm -f outfile2
