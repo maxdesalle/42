4ab4fdcd24084a6d986f125a60fa6022eb2f338e
maxdesalle <maxdesalle@pm.me>
2021-08-04 09:36:09 +0200

    added some fancy colors

--- pipex/test/tester.sh
+++ pipex/test/tester.sh
@@ -1,34 +1,29 @@
 #!/bin/bash
 
 # Some nice little colors
-RED="\e[91m"
-GREEN="\e[92m"
-YELLOW="\e[93m"
-BLUE="\e[94m"
-PURPLE="\e[95m"
-CYAN="\e[96m"
-WHITE="\e[97m"
-
-echo -n "baracuda\ncrocodile\nelephant\n" > file1
-echo -n "baracuda\ncrocodile\nelephant\n" > file2
+RED="\033[91m"
+WHITE="\033[97m"
 
 < README.md ls -la | grep pipex > outfile1
 ./pipex README.md "ls -la" "grep pipex" outfile2
+echo -en $RED
 diff outfile1 outfile2
+echo -en $WHITE
 rm -f outfile1
 rm -f outfile2
 
 < README.md grep is | wc -l > outfile1
 ./pipex README.md "grep is" "wc -l" outfile2
+echo -en $RED
 diff outfile1 outfile2
+echo -en $WHITE
 rm -f outfile1
 rm -f outfile2
 
 < README.md cat | wc -w > outfile1
 ./pipex README.md cat "wc -w" outfile2
+echo -en $RED
 diff outfile1 outfile2
+echo -en $WHITE
 rm -f outfile1
 rm -f outfile2
-
-rm -f file1
-rm -f file2
