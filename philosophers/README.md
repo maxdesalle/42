0fa43a4b33c0fcb5218669944de82cc84f4a9f76
maxdesalle <maxdesalle@pm.me>
2021-08-06 13:03:58 +0200

    start of philosophers

---
+++ philosophers/README.md
@@ -0,0 +1,7 @@
+# ```philosophers```
+
+## Warning
+The structure of this repository does **not** necessarily follow the guidelines.
+
+## License
+Everything in this repository is released under the [Unlicense](https://github.com/maxdesalle/42/blob/main/LICENSE).
---
+++ philosophers/include/philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdesalle <mdesalle@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/06 09:36:10 by mdesalle          #+#    #+#             */
+/*   Updated: 2021/08/06 11:30:03 by mdesalle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+# define STDIN 0
+# define STDOUT 1
+# define STDERR 2
+
+#endif
