fb00bde9a715eed37a46860b4990e563f0a95ef1
maxdesalle <maxdesalle@pm.me>
2021-11-19 11:19:57 +0100

    start of ft_container

---
+++ ft_container/containers/vector.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maxdesalle <mdesalle@student.s19.be>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/19 11:03:03 by maxdesall         #+#    #+#             */
+/*   Updated: 2021/11/19 11:19:47 by maxdesall        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VECTOR_HPP
+# define VECTOR_HPP
+
+namespace ft
+{
+	template<class T, class Allocator = std::allocator<T>>
+	class vector
+	{
+		public:
+
+		private:
+	};
+}
+
+#endif
